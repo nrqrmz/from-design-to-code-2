5262126863c7c58f33a2c80242deb7fd06460198
Enrique Ramirez <nrqrmz@gmail.com>
2022-08-30 07:01:26 -0500

    fix issues

--- app/javascript/controllers/search_movies_controller.js
+++ app/javascript/controllers/search_movies_controller.js
@@ -1,7 +1,11 @@
 import { Controller } from "@hotwired/stimulus"
 
-// Connects to data-controller="search-movies"
 export default class extends Controller {
+  static targets = ["form", "input", "list"]
+
   connect() {
+    console.log(this.formTarget)
+    console.log(this.inputTarget)
+    console.log(this.listTarget)
   }
 }
