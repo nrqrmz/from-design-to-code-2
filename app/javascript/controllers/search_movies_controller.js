7cf48d25486afcbae721072c590ae2d9139a102d
Enrique Ramirez <nrqrmz@gmail.com>
2022-08-30 07:04:06 -0500

    connect update action

--- app/javascript/controllers/search_movies_controller.js
+++ app/javascript/controllers/search_movies_controller.js
@@ -3,9 +3,13 @@ import { Controller } from "@hotwired/stimulus"
 export default class extends Controller {
   static targets = ["form", "input", "list"]
 
-  connect() {
-    console.log(this.formTarget)
-    console.log(this.inputTarget)
-    console.log(this.listTarget)
+  // connect() {
+  //   console.log(this.formTarget)
+  //   console.log(this.inputTarget)
+  //   console.log(this.listTarget)
+  // }
+
+  update() {
+    console.log("TODO: send request in AJAX")
   }
 }
